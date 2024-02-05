1374248c3d41324001b78de07b18d86f56068254
monika kathiriya <kathiriyamonika27@gmail.com>
2024-02-05 11:01:00 +0530

    05-02-2024

---
+++ intro.sql
@@ -0,0 +1,10 @@
+create table product(
+    ProductNo varchar(6),
+    Description varchar(15),
+    Profitpercent int,
+    Unitmeasure varchar(10),
+    Qtyonhand int,
+    Reorderlvl int,
+    Sellprice int,
+    Costprice int
+);
