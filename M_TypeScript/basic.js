66e9a4cdf175a744daa5db3b8cb64fdd666b294d
monika kathiriya <kathiriyamonika27@gmail.com>
2024-02-07 09:29:06 +0530

    07-02-2023

--- M_TypeScript/basic.js
+++ M_TypeScript/basic.js
@@ -1,2 +1,18 @@
-var a = 10;
-console.log(a);
+// let a=10;
+// console.log(a);
+// let a: number = 1025;
+// // b = 'hello';
+// console.log(a);
+// let x : number | string = 101;
+// // x = 'hello';
+// console.log(x);
+// let a : string[] = ["Hello"]
+// // let a : Array<string> = [Hello]
+// a[1] = 'skill';
+// a[20] = "101";
+// console.log(a);
+var x;
+x = ['hello', 10, 20, 'soq'];
+// x.push("qode", 20);
+// x.push(50, "qode");
+console.log(x);
--- M_TypeScript/basic.ts
+++ M_TypeScript/basic.ts
@@ -1,2 +1,25 @@
-let a=10;
-console.log(a);
+// let a=10;
+// console.log(a);
+
+// let a: number = 1025;
+// let b = 'hello';
+// console.log(a);
+// console.log(b);
+
+// let x : number | string = 101;
+// // x = 'hello';
+// console.log(x);
+
+
+// let a : string[] = ["Hello"]
+// // let a : Array<string> = [Hello]
+
+// a[1] = 'skill';
+// a[20] = "101";
+// console.log(a);
+
+// let x : [string, number, number, string];
+// x = ['hello', 10, 20, 'soq'];
+// // x.push("qode", 20);
+// // x.push(50, "qode");
+// console.log(x);
