a8f00b3299b5874b57004e689d40a7ebdd78e939
monika kathiriya <kathiriyamonika27@gmail.com>
2024-02-16 09:49:06 +0530

    16-02-2024

---
+++ M_Node.Js/path.js
@@ -0,0 +1,29 @@
+// *-------PATH MODULE-------*
+
+const path = require('path');
+
+// console.log(path.basename('D:/nodejs/file.js'));
+// console.log(path.dirname('D:/nodejs/file.js'));
+// console.log(path.extname('D:/nodejs/file.js'));
+// console.log(path.isAbsolute('D:/nodejs/file.js'));
+// console.log(path.normalize('D:/nodejs/file.c'));
+// console.log(path.parse('D:/nodejs/file.c'));
+// console.log(path.format({
+//        root: 'C:',
+//        dir: 'C:/NodeJS',
+//        base: 'hello.js',
+//        ext: '.js'
+// }));
+
+// console.log(path.posix);
+// console.log(path.delimiter);
+// console.log(path.sep);
+// console.log(path.relative("D:/nodejs/","file.js"));
+// console.log(path.resolve("D:/nodejs/file.c"));
+// console.log(path.toNamespacedPath("D:/nodejs/file.c"));
+
+
+// console.log(path.join("D:", "NodeJS", "Hello.js", "abc"));
+// console.log(path.join(__dirname, "Hello.js", "abc"));
+// console.log(path.basename(__dirname));
+// console.log(path.basename(__filename));
