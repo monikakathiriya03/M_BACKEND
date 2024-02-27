d53a248b9a65c7725802c938f7c05b4b2c8aec52
monika kathiriya <kathiriyamonika27@gmail.com>
2024-02-27 11:33:41 +0530

    27-02-2024

--- EXPRESS/app.js
+++ EXPRESS/app.js
@@ -1,30 +1,67 @@
+// const express = require('express');
+// const app = express();    // server create
+// const port = 2828;
+// const morgan = require('morgan');
+
+// //middleware
+
+// let myFun = (req, res, next) =>{
+//     // console.log(req.query);
+
+//     if((req.query.age)>=18){
+//         next();
+//     }else{
+//         res.send("Sorry, you have under below 18");
+//     }
+// }
+
+// app.use(myFun); // Application Level Middleware
+// // app.use(express.json()); //built-in middleware
+// app.use(morgan('dev')); //morgan middleware
+// app.get('/',myFun,(req, res) =>{
+//     res.send('Welcome to Express JS');
+// });
+// app.post('/',(req, res) =>{
+//     res.send('Post Method');
+// });
+// app.listen(port,()=>{
+//     console.log('Server Start at http://localhost:2828');
+// });
+           
+
+
+//*---------27-02-2024----------*
+
 const express = require('express');
 const app = express();    // server create
 const port = 2828;
 const morgan = require('morgan');
-
-//middleware
+const path = require('path');
 
 let myFun = (req, res, next) =>{
-    // console.log(req.query);
-
-    if((req.query.age)>=18){
-        next();
-    }else{
-        res.send("Sorry, you have under below 18");
+        // console.log(req.query);
+    
+        if((req.query.age)>=18){
+            next();
+        }else{
+            res.send("Sorry, you have under below 18");
+        }
     }
-}
 
-app.use(myFun); // Application Level Middleware
-// app.use(express.json()); //built-in middleware
-app.use(morgan('dev')); //morgan middleware
-app.get('/',myFun,(req, res) =>{
-    res.send('Welcome to Express JS');
-});
-app.post('/',(req, res) =>{
-    res.send('Post Method');
-});
-app.listen(port,()=>{
-    console.log('Server Start at http://localhost:2828');
-});
-           
+    // app.use(express.json()); // built-in 
+    // app.use(express.urlencoded({ extended: true })); // built-in
+    app.use('/hello',express.static(path.join(__dirname, 'public')));  // built-in
+
+    app.use(morgan('dev'));
+    app.get('/',myFun,(req, res) =>{
+        // console.log(req.body);
+        res.send('about');
+    });
+    app.post('/',(req, res) =>{
+        console.log(req.body);
+        res.send('Post Method');
+    });
+
+    app.listen(port,()=>{
+        console.log('Server Start at http://localhost:2828');
+    });
