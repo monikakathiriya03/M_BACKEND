5750f1b66e7721198c25cd89bf2a724168758c83
monika kathiriya <kathiriyamonika27@gmail.com>
2024-02-29 08:11:58 +0530

    28-02-2024

--- EXPRESS/app.js
+++ EXPRESS/app.js
@@ -32,44 +32,84 @@
 
 //*---------27-02-2024----------*
 
-const express = require('express');
-const app = express();    // server create
-const port = 2828;
-const morgan = require('morgan');
-const path = require('path');
+// const express = require('express');
+// const app = express();    // server create
+// const port = 2828;
+// const morgan = require('morgan');
+// const path = require('path');
 
-let myFun = (req, res, next) =>{
-        // console.log(req.query);
+// let myFun = (req, res, next) =>{
+//         // console.log(req.query);
     
-        if((req.query.age)>=18){
-            next();
-        }else{
-            res.send("Sorry, you have under below 18");
-        }
-    }
+//         if((req.query.age)>=18){
+//             next();
+//         }else{
+//             res.send("Sorry, you have under below 18");
+//         }
+//     }
 
-    // app.use(express.json()); // built-in 
-    // app.use(express.urlencoded({ extended: true })); // built-in
-    app.use('/hello',express.static(path.join(__dirname,'public')));  // built-in
+//     // app.use(express.json()); // built-in 
+//     // app.use(express.urlencoded({ extended: true })); // built-in
+//     app.use('/hello',express.static(path.join(__dirname,'public')));  // built-in
     
-    app.use(morgan('dev'));
-    app.get('/',myFun,(req, res) =>{
-        // console.log(req.body);
-        res.send('about');
-    });
+//     app.use(morgan('dev'));
+//     app.get('/',myFun,(req, res) =>{
+//         // console.log(req.body);
+//         res.send('about');
+//     });
 
-//     app.use(express.static(path.join(__dirname, 'public')));
-// app.get('/page', (req, res) => {
-//     // Read the HTML file
-//     const htmlPath = path.join(__dirname, 'public', 'index.html');
-//     res.sendFile(htmlPath);
-// });
+// //     app.use(express.static(path.join(__dirname, 'public')));
+// // app.get('/page', (req, res) => {
+// //     // Read the HTML file
+// //     const htmlPath = path.join(__dirname, 'public', 'index.html');
+// //     res.sendFile(htmlPath);
+// // });
     
-    app.post('/',(req, res) =>{
-        console.log(req.body);
-        res.send('Post Method');
-    })
+//     app.post('/',(req, res) =>{
+//         console.log(req.body);
+//         res.send('Post Method');
+//     })
 
-    app.listen(port,()=>{
-        console.log('Server Start at http://localhost:2828');
-    });
+//     app.listen(port,()=>{
+//         console.log('Server Start at http://localhost:2828');
+//     });
+
+
+//*---------28-02-2024----------*
+
+ const express = require('express');
+ const app = express();
+ const port = 2828;
+ const morgan = require('morgan');
+ const Products = require('./public/products.json');
+//  console.log(products);
+
+ //Middleware
+ app.use(express.json());
+ app.use(morgan('dev'));
+
+ //End-points - CRUD
+ app.post('/products',(req, res)=>{
+    //  console.log(req.body);
+     const product = req.body;
+     Products.push(product);
+     // products.push({...req.body});
+     res.status(201).json({message: 'Product is added....'});
+ });
+
+ app.get('/products',(req, res)=>{
+     res.status(200).json(Products);
+ });
+
+
+ app.get('/products/single-product',(req, res)=>{
+    const id = +req.query.id;
+    console.log(id);
+    let product = Products.find((item)=>item.id === id);
+    res.status(200).json(product);
+});
+
+
+app.listen(port,()=>{
+    console.log('Server Start at http://localhost:2828');
+});
