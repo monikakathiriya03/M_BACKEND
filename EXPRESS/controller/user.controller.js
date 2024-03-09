7e19126a7ca301ff88b26da96ea4158172946fae
monika kathiriya <kathiriyamonika27@gmail.com>
2024-03-09 09:31:39 +0530

    08-03-2024

--- EXPRESS/controller/user.controller.js
+++ EXPRESS/controller/user.controller.js
@@ -1,24 +1,51 @@
 const User = require('../model/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-exports.addUser = async (req, res) => {
+exports.registerUser = async (req, res) => {
     try {
        const {firstName, lastName, gender, email, password, age} = req.body; 
-       console.log(req.body);
-       let newUser = await User.create({
+    //    console.log(req.body);
+    let user = await User.findOne({ email: email, isDelete: false });
+    if(user){
+        return res.status(400).json({ message:'User is already registered...'});
+    }
+    // hash password
+    let hashPassword =await bcrypt.hash(password, 10);
+       user = await User.create({
            firstName,
            lastName,
            gender,
            email,
-           password,
+           password: hashPassword,
            age
     });
-    newUser.save();
-    res.status(201).json({user: newUser, Message: 'New User Added Successfully...'});
+    user.save();
+    res.status(201).json({user: user, Message: 'New User Added Successfully...'});
     } catch (error) {
         console.log(error);
         res.status(500).json({message: 'Internal Server Error'});   
     }
-}
+};
+
+exports.loginUser = async (req,res) => {
+    try{
+        let user = await User.findOne({ email: req.body.email, isDelete: false });
+        if (!user){
+            return res.status(404).json({message: 'User Not Found'});
+        }
+        let chaekPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!chaekPassword){
+            return res.status(400).json({message: 'Password is not match..'});
+        }
+        let token = jwt.sign({userId: user._id}, 'SkillQode');
+        res.status(200).json({token, message: 'Login Successful'});
+    }
+     catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Internal Server Error'});
+    }
+};
 
 exports.getAllUsers = async (req, res) => {
     try {
